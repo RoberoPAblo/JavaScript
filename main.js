71844d33dddb8a40ffe89850c1849a15f2d05dc9
RobertoPablo <99889624+RoberoPAblo@users.noreply.github.com>
2022-03-24 08:00:42 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
  var btn1= document.querySelector(".btn1")
  var btn2= document.querySelector("#btn2")
+ var res= document.querySelector("#res")
+ var dato1= 5
+ var dato2= 10
+ var respuesta= dato1 + dato2
+ 
+
+ console.log(respuesta);
 
     btn1.addEventListener("click", showname)
     btn2.addEventListener("click", showlastname)
 
     function showname(){
       var name= document.querySelector("#name")
-      alert("El nombre es: "+ name.value)
+      res.innerHTML= name.value
+      
     }
 
     function showlastname(){
         var lastname= document.querySelector("#lastname")
         alert("el Apellido es:"+ lastname.value)
+        
     }
